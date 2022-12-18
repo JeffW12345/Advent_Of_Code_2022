5f2d2bd711be145753d720771a3971ac258322c3
Jeff Waters <watersjg@gmail.com>
2022-12-18 14:35:44 +0000

    Corrected error

--- src/main/java/Day16Part1/Day16Part1.java
+++ src/main/java/Day16Part1/Day16Part1.java
@@ -3,7 +3,7 @@ package Day16Part1;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-public class Day8Part1 {
+public class Day16Part1 {
 
     public static void main(String[] args) throws MalformedURLException {
         System.out.println(new URL("https://adventofcode.com/2022/day/16"));
